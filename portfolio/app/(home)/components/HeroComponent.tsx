b0cbbcf24b42c5a5731af203d8e0fc859f672eb1
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-10 18:04:42 +0530

    Added glow effect

--- portfolio/app/(home)/components/HeroComponent.tsx
+++ portfolio/app/(home)/components/HeroComponent.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function HeroComponent() {
   return (
-      <div className='min-h-[60vh] flex items-center justify-between'>
-          <div className='space-y-10'>
+      <div className='min-h-[60vh] flex flex-col-reverse gap-14 lg:gap-0 lg:flex-row items-center justify-between'>
+          <div className='space-y-10 text-center lg:text-left'>
               <h1 className='text-4xl lg:text-7xl font-bold'>Namaste!🙏🏽 <br />
                   <span className='underline underline-offset-8 decoration-blue-500'>{"I'm Aakash."}</span>
               </h1>
@@ -13,16 +13,16 @@ export default function HeroComponent() {
                       "Based in India, I'm a Fullstack developer passionate about building a modern web application that users love."
                   }
               </p>
-              <Link href={"mailto:aakash.ar1213@gmail.com"} className='inline-block'>
+              <Link href={"mailto:aakash.ar1213@gmail.com"} className='inline-block group'>
                   <div>
-                      <h1 className='font-bold text-3xl hover:text-blue-500'> Contact Me📬</h1>
-                      <div className='w-full h-2 bg-blue-500 rounded-full'></div>
-                      <div className='w-full h-2 bg-purple-500 rounded-full translate-x-2'></div>
+                      <h1 className='font-bold text-3xl group-hover:text-blue-500 transition-all'> Contact Me📬</h1>
+                      <div className='w-40 h-2 bg-blue-500 rounded-full'></div>
+                      <div className='w-40 h-2 bg-purple-500 rounded-full translate-x-2'></div>
              </div>
               </Link>
           </div>
           <div>
-              <div className='w-72 h-72 space-y-3 -rotate-[30deg]'>
+              <div className='w-72 h-72 space-y-3 -rotate-[30deg] relative'>
                 <div className='flex gap-3 translate-x-8'>
                   <div className='w-32 h-32 rounded-2xl bg-blue-500'></div>
                     <div className='w-32 h-32 rounded-full bg-purple-500'></div>
@@ -30,7 +30,8 @@ export default function HeroComponent() {
                 <div className='flex gap-3 -translate-x-8'>
                     <div className='w-32 h-32  rounded-2xl bg-purple-500'></div>
                     <div className='w-32 h-32  rounded-full bg-blue-500'></div>
-                 </div>
+                  </div>
+                  <div className="glow absolute top-[40%] right-1/2 -z-10"></div>
               </div>
           </div>
     </div>
--- portfolio/app/(home)/page.tsx
+++ portfolio/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import HeroComponent from './components/HeroComponent'
 export default function page() {
   return (
     <div className='min-h-screen bg-black'>
-      <div className='max-w-7xl mx-auto'>
+      <div className='max-w-7xl mx-auto p-5'>
         <Navbar />
         <HeroComponent/>
       </div>
