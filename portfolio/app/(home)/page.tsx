b475a07e68b567309cbb16f817567484d2de2af6
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-10 17:11:28 +0530

    Commiting first

---
+++ portfolio/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='min-h-screen bg-black'>
+      <div className='max-w-7xl mx-auto'>
+        <nav>
+          
+        </nav>
+      </div>
+      </div>
+  )
+}
