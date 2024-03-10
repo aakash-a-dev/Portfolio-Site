c6a053536319faf55c4cfdee07e2d8cc80df9191
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-10 18:12:38 +0530

    Added Grid using Acernity UI in Hero Component

--- portfolio/app/(home)/page.tsx
+++ portfolio/app/(home)/page.tsx
@@ -4,11 +4,13 @@ import HeroComponent from './components/HeroComponent'
 
 export default function page() {
   return (
-    <div className='min-h-screen bg-black'>
-      <div className='max-w-7xl mx-auto p-5'>
+    <div className='min-h-screen bg-black overflow-hidden'>
+      <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2]'>
+      <div className='max-w-7xl mx-auto p-5 '>
         <Navbar />
         <HeroComponent/>
-      </div>
+        </div>
+        </div>
       </div>
   )
 }
