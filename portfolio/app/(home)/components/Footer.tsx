83c056bc9683c63d5151fe473440cffc67fe4695
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-11 16:41:38 +0530

    Added New Favicon

--- portfolio/app/(home)/components/Footer.tsx
+++ portfolio/app/(home)/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
       <div className='justify-end items-center'>
           <div className=''>
-              <p className=''>Made with ❤️ By <span><Link href="https://www.linkedin.com/in/aakash-a-rajput/">AAKASH AMOD RAJPUT</Link></span> </p>
+              <p className='text-center'>Made with ❤️ By <span><Link href="https://www.linkedin.com/in/aakash-a-rajput/">AAKASH AMOD RAJPUT</Link></span> </p>
               </div>
         </div>
     
