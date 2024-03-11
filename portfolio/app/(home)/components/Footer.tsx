0fca13457d5541900a8da29d5dda85784a6100aa
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-11 16:28:25 +0530

    Made a rough Footer for now

---
+++ portfolio/app/(home)/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Link from 'next/link';
+
+export default function Footer() {
+  return (
+      <div className='justify-end items-center'>
+          <div className=''>
+              <p className=''>Made with ❤️ By <span><Link href="https://www.linkedin.com/in/aakash-a-rajput/">AAKASH AMOD RAJPUT</Link></span> </p>
+              </div>
+        </div>
+    
+  )
+}
--- portfolio/app/(home)/page.tsx
+++ portfolio/app/(home)/page.tsx
@@ -3,10 +3,11 @@ import Navbar from './components/Navbar'
 import HeroComponent from './components/HeroComponent'
 import { MovingBorderBtn } from '@/components/ui/MovingBorderBtn'
 import Skills from './components/Skills'
+import Footer from './components/Footer'
 
 export default function page() {
   return (
-    <div className='min-h-[400vh] bg-black overflow-hidden'>
+    <div className='min-h-full bg-black overflow-hidden'>
     <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
       <div className='max-w-7xl mx-auto p-5 '>
         <Navbar />
@@ -18,7 +19,10 @@ export default function page() {
         
     <div className='max-w-7xl mx-auto p-5 mt-20'>
       <Skills />
-    </div>
+      </div>
+      <div className='max-w-7xl mx-auto p-5 mt-20 bg-slate-900'>
+        <Footer />
+       </div> 
       </div>
   )
 }
