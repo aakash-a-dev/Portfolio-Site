f38c56fb07290d37a10ef8975534970994cd3789
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-12 23:21:00 +0530

    Project section looking better now

--- portfolio/app/(home)/components/Projects.tsx
+++ portfolio/app/(home)/components/Projects.tsx
@@ -19,26 +19,32 @@ export default function Projects() {
             tech: [SiNodedotjs, SiReact],
             link: "https://chimerical-taffy-43d337.netlify.app/",
             cover: "/Attendance.png",
-            background: "bg-purple-500"
+            background: "bg-blue-500"
         },
           {
             title: "Dummy Money Transaction System",
             tech: [SiNodedotjs, SiReact, SiMongodb],
             link: "https://github.com/aakash-a-dev/money-transaction-system",
             cover: "/Money.jpeg",
-            background: "bg-purple-500"
+            background: "bg-green-500"
         },
 
     ]
   return (
       <div className='py-10 p-5 sm:p-0'>
           <Title text='Projects🚀' className='flex flex-col items-center justify-center -rotate-6' />
-          <div className="grid grid-cols-2 sm:grid-cols-2 pt-20">
+          <div className="grid grid-cols-2 sm:grid-cols-2 pt-20 gap-5">
               {Project.map((project, index) => {
+                  const Icon = project.tech;
                   return <Link href={project.link} key={index}>
                       <div className={cn("p-5 rounded-md", project.background)}>
                           <DirectionAwareHover imageUrl={project.cover} className='w-full space-y-5 cursor-pointer'>
                               <h1>{project.title}</h1>
+                              <div className='flex items-center gap-5'>
+                                  {project.tech.map((Icon, index) => {
+                                      return <Icon key={index} />
+                                  })}
+                              </div>
                           </DirectionAwareHover>
                       </div>
                   </Link>
