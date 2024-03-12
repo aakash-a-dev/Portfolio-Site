fc54282d186ae15506a8a694f7cca4529ebd77c6
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-12 22:59:14 +0530

    Added list for Project

--- portfolio/app/(home)/components/Projects.tsx
+++ portfolio/app/(home)/components/Projects.tsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import { SiAuth0, SiLaravel, SiMongodb, SiNodedotjs, SiPostgresql, SiReact } from 'react-icons/si'
 
 export default function Projects() {
+    const Project = [
+        {
+            title: "Kresoon Social",
+            tech: [SiLaravel, SiPostgresql, SiAuth0],
+            link: "https://krescon.social/",
+            cover: "/Krescon.png",
+            background: "bg-purple-500"
+        },
+         {
+            title: "AI Automated Attendance System",
+            tech: [SiNodedotjs, SiReact],
+            link: "https://chimerical-taffy-43d337.netlify.app/",
+            cover: "/Attendance.png",
+            background: "bg-purple-500"
+        },
+          {
+            title: "Dummy Money Transaction System",
+            tech: [SiNodedotjs, SiReact, SiMongodb],
+            link: "https://github.com/aakash-a-dev/money-transaction-system",
+            cover: "/Money.jpeg",
+            background: "bg-purple-500"
+        },
+
+    ]
   return (
     <div>Projects</div>
   )
