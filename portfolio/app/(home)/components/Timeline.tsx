9d5f587b9a092368da5ed15ae517567463eb8550
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-13 21:44:19 +0530

    Added Hover effect

--- portfolio/app/(home)/components/Timeline.tsx
+++ portfolio/app/(home)/components/Timeline.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import Title from './Title'
+import { TextRevealCard, TextRevealCardDescription, TextRevealCardTitle } from '@/components/ui/text-reveal-card'
 
 export default function Timeline() {
-  return (
-          <Title text='My Previous Work Experience🤺' className='flex flex-col items-center justify-center -rotate-6' />
-    
+    return (
+        <>
+            <div className='max-w-5xl mx-auto px-8 pt-8 '>
+      <Title text='My Previous Work Experience🤺' className='flex flex-col items-center justify-center -rotate-6' />
+        <div className="pt-8 flex justify-center items-center">
+      <TextRevealCard
+        text="You know the business"
+        revealText="I know the chemistry "
+      >
+        <TextRevealCardTitle>
+          Why me?
+        </TextRevealCardTitle>
+        <TextRevealCardDescription>
+          Just because...
+        </TextRevealCardDescription>
+      </TextRevealCard>
+                </div>
+                </div>
+            </>
   )
 }
