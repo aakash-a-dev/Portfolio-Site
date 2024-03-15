9665971497c58b60b563c67db1c557105e68667c
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-15 16:18:51 +0530

    Working on timeline

--- portfolio/app/(home)/components/Timeline.tsx
+++ portfolio/app/(home)/components/Timeline.tsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Title from './Title'
 import { TextRevealCard, TextRevealCardDescription, TextRevealCardTitle } from '@/components/ui/text-reveal-card'
 
-export default function Timeline() {
+
+  const events = [
+    {
+      company: "Boostr Technology",
+      position: "Flutter Developer Intern",
+      timePeriod: "Aug-2022 to Oct-2022"
+    },
+     {
+      company: "Boostr Technology",
+      position: "Flutter Developer Intern",
+      timePeriod: "Aug-2022 to Oct-2022"
+    },
+      {
+      company: "Boostr Technology",
+      position: "Flutter Developer Intern",
+      timePeriod: "Aug-2022 to Oct-2022"
+    },
+
+  ]
+export default function Timeline({ events }: { events: any }) {
+
     return (
         <>
             <div className='max-w-5xl mx-auto px-8 pt-8 '>
       <Title text='My Previous Work Experience🤺' className='flex flex-col items-center justify-center -rotate-6' />
-     </div>
+        </div>
+        <div className='flex flex-col gap-y-3 w-full my-4'>
+          <Circle />
+          {events.map((event, key) => {
+            <Fragment key={key}>
+              <div className='grid gird-cols-[1fr-auto-1fr] gap-x-2 items-center mx-auto'>
+                {event.direction === 'left' ?
+                  (<EventCard heading={event.company} subheading={event.position} />) :
+                  (
+                    <div></div>
+                  )
+                }
+              </div>
+            </Fragment>
+          })}
+          <Circle/>
+        </div>
             </>
   )
 }
